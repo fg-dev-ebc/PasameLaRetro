dd40f3d89ad013b5823179895741bf579fdf6501
state-of-dev <lfgxxo@gmail.com>
2026-05-09 18:51:41 -0600

    deploy

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -23,7 +23,7 @@ export default function Image() {
         }}
       >
         <img
-          src="https://pasamelaretro.vercel.app/pasamelaretro-wsp.png"
+          src="https://pasamelaretro.vercel.app/pasamelaretro-logo2.png"
           alt="Pasame La Retro"
           style={{
             maxHeight: "440px",
